99ef38679f9d16568c803f02a2b432cc93781fde
Vadim Timonin <tim6693@gmail.com>
2020-10-25 15:14:37 +0300

    Add tiering node

--- conf.sh
+++ conf.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-for i in {1..5}; do ssh-copy-id node$i; done
+for i in {1..6}; do ssh-copy-id node$i; done
 
-for i in {1..5}; do ssh node$i "sudo openssl genrsa -out /tmp/glusterfs.key 2048"; done
+for i in {1..6}; do ssh node$i "sudo openssl genrsa -out /tmp/glusterfs.key 2048"; done
 
-for i in {1..5}; do ssh node$i sudo openssl req -new -x509 -key /tmp/glusterfs.key -subj "/CN=$(hostname)" -out /tmp/glusterfs.pem; done
+for i in {1..6}; do ssh node$i sudo openssl req -new -x509 -key /tmp/glusterfs.key -subj "/CN=$(hostname)" -out /tmp/glusterfs.pem; done
 
-for i in {1..5}; do scp root@node$i:/tmp/glusterfs.pem /tmp/node$i.pem; done
+for i in {1..6}; do scp root@node$i:/tmp/glusterfs.pem /tmp/node$i.pem; done
 
-for i in {1..5}; do scp root@node$i:/tmp/glusterfs.key /tmp/node$i.key; done
+for i in {1..6}; do scp root@node$i:/tmp/glusterfs.key /tmp/node$i.key; done
 
 for i in {1..4}; do ssh node$i "rm -f /tmp/glustefs*"; done
 
 ### this file is for all server nodes only ###
-cat /tmp/node1.pem /tmp/node2.pem /tmp/node3.pem /tmp/node4.pem /tmp/node5.pem > /tmp/glusterfs.ca
+cat /tmp/node1.pem /tmp/node2.pem /tmp/node3.pem /tmp/node4.pem /tmp/node5.pem /tmp/node6.pem > /tmp/glusterfs.ca
 
 for i in 5;
 do
 	yum install -y httpd
 	systemctl enable httpd
 	systemctl start httpd
-	echo "This ${hostname} is used as FTP server and provides *.pem *.key glusterfs.ca files" > /var/www/html/index.html
+	echo "This $(hostname) is used as FTP server and provides glusterfs.pem glusterfs.key glusterfs.ca files" > /var/www/html/index.html
 	systemctl restart httpd
 done
 
