ff67f29218e96fb76db83d969acbef92f43c34dc
root <root@node5.example.com>
2020-10-25 23:07:40 +0000

    New CA script

--- conf.sh
+++ conf.sh
@@ -1,28 +1,50 @@
 #!/bin/bash
-for i in {1..6}; do ssh-copy-id node$i; done
 
-for i in {1..6}; do ssh node$i "sudo openssl genrsa -out /tmp/glusterfs.key 2048"; done
+#set ssh-key for hosts
+for i in {1..6}; do ssh-copy-id -o StrictHostKeyChecking=no node$i; done
 
-for i in {1..6}; do ssh node$i sudo openssl req -new -x509 -key /tmp/glusterfs.key -subj "/CN=$(hostname)" -out /tmp/glusterfs.pem; done
+#generate .key and *pem files for servers only
+for i in 1 2 3 4 6; do ssh node$i "sudo openssl genrsa -out /tmp/node${i}.key 2048"; done
+for i in 1 2 3 4 6; do ssh node$i sudo openssl req -new -x509 -key /tmp/node$i.key -subj "/CN=$(hostname)" -out /tmp/node$i.pem; done
 
-for i in {1..6}; do scp root@node$i:/tmp/glusterfs.pem /tmp/node$i.pem; done
+#generate .key and *pem files for client only
+sudo openssl genrsa -out /tmp/client.key 2048
+sudo openssl req -new -x509 -key /tmp/client.key -subj "/CN=$(hostname)" -out /tmp/client.pem
 
-for i in {1..6}; do scp root@node$i:/tmp/glusterfs.key /tmp/node$i.key; done
+#create CA file
 
-for i in {1..4}; do ssh node$i "rm -f /tmp/glustefs*"; done
+scp root@node1:/tmp/node1.pem /tmp/
+scp root@node1:/tmp/node1.key /tmp/
 
-### this file is for all server nodes only ###
-cat /tmp/node1.pem /tmp/node2.pem /tmp/node3.pem /tmp/node4.pem /tmp/node5.pem /tmp/node6.pem > /tmp/glusterfs.ca
+scp root@node2:/tmp/node2.pem /tmp/
+scp root@node2:/tmp/node2.key /tmp/
 
+scp root@node3:/tmp/node3.pem /tmp/
+scp root@node3:/tmp/node3.key /tmp/
+
+scp root@node4:/tmp/node4.pem /tmp/
+scp root@node4:/tmp/node4.key /tmp/
+
+scp root@node6:/tmp/node6.pem /tmp/
+scp root@node6:/tmp/node6.key /tmp/
+
+#for servers
+cat /tmp/node1.pem /tmp/node2.pem /tmp/node3.pem /tmp/node4.pem /tmp/node6.pem /tmp/client.pem > /tmp/glusterfs-servers.ca
+
+#for client
+cat /tmp/node1.pem /tmp/node2.pem /tmp/node3.pem /tmp/node4.pem /tmp/node6.pem > /tmp/glusterfs-client.ca
+
+#copy to /var/www/html for downloading
 for i in 5;
 do
 	yum install -y httpd
+	yum remove glusterfs* -y
 	systemctl enable httpd
 	systemctl start httpd
 	echo "This $(hostname) is used as FTP server and provides glusterfs.pem glusterfs.key glusterfs.ca files" > /var/www/html/index.html
 	systemctl restart httpd
 done
 
-cp /tmp/node*.key /var/www/html/
-cp /tmp/node*.pem /var/www/html/
-cp /tmp/glusterfs.ca /var/www/html/
+cp /tmp/*.key /var/www/html/
+cp /tmp/*.pem /var/www/html/
+cp /tmp/*.ca /var/www/html/
